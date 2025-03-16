4c9f27d00100e6a1ea58de3a10125f3c811a8b6f
mehedifiz <mehedi.shanto.contract@gmail.com>
2025-03-16 23:35:54 +0600

    ok

--- config/mongodb.js
+++ config/mongodb.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
 	try {
 		await mongoose.connect(process.env.MONGODB_URI, {
-			dbName: "ayotto",
+			dbName: "SRS",
 		});
 
 		console.log("Database connected successfully");
