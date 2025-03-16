dd4ebf3767f56e25395c1c43bccdf88384ca5852
mehedifiz <mehedi.shanto.contract@gmail.com>
2025-03-17 00:53:45 +0600

    updated

--- models/User/userModel.js
+++ models/User/userModel.js
@@ -1,22 +1,52 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  phone: { type: Number, unique: true }, // Added phone field
-  password: String,
-  role: { type: String, enum: ["student", "admin"], default: "student" },
-  subscription: {
-    plan: { type: String, enum: ["basic", "standard", "premium"], default: "basic" },
-    expiresAt: Date,
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+   
+    unique: true,
+    trim: true
+  },
+  phone: { 
+    type: Number, 
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: { 
+    type: String, 
+    enum: ["student", "admin"], 
+    default: "student" 
+  },
+  subscription: { 
+    type: String,
+    enum: ["basic", "standard", "premium"],
+    default: "basic"
   },
   progress: [
     {
-      chapterId: mongoose.Schema.Types.ObjectId,
-      moduleId: mongoose.Schema.Types.ObjectId,
-      completed: Boolean,  
+      chapterId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Chapter"
+      },
+      moduleId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Module"
+      },
+      completed: {
+        type: Boolean,
+        default: false
+      }
     }
-  ],
+  ]
 }, { timestamps: true });
- 
+
 export const User = mongoose.model("User", userSchema);
