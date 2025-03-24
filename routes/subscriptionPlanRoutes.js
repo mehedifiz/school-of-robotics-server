3fcfdd24b82202fd255ba563af413c6bab22c7ca
MD Arman khan <mdarmankhan6252@gmail.com>
2025-03-25 01:31:39 +0600

    I have created two route that is get single plan and delete a single plan router

--- routes/subscriptionPlanRoutes.js
+++ routes/subscriptionPlanRoutes.js
@@ -2,7 +2,9 @@ import express from 'express';
 import auth from '../middleware/authMiddleware.js';
 import { 
   createPlan, 
+  deletePlan, 
   getAllPlans, 
+  getAPlan, 
   updatePlan 
 } from '../controllers/subscriptionPlanController.js';
 
@@ -11,8 +13,9 @@ const router = express.Router();
 // Admin route
 router.post('/create', auth('admin'), createPlan);
 router.patch('/update/:id', auth('admin'), updatePlan);
-
+router.delete('/delete/:id', auth('admin'), deletePlan);
 // Public route
 router.get('/all', getAllPlans);
+router.get('/:id', getAPlan)
 
 export default router;
